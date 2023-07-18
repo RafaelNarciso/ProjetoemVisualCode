13d5e2d07b75e6c63d87556a7e6357c372080e08
RafaelNarciso <rafaelnarcisodasilva@gmail.com>
2023-07-18 11:12:52 -0300

    subindo codigo

---
+++ src/pacote01/dog.java
@@ -0,0 +1,8 @@
+package pacote01;
+
+public class dog {
+    public void bark() {
+        System.out.println("AuAU");
+    }
+
+}
---
+++ src/pacote04/programa04.java
@@ -0,0 +1,15 @@
+package pacote04;
+
+public class programa04 {
+    public static void main(String[] args) {
+
+        int numero = 7;
+
+        if (numero > 5) {
+            System.out.println("Sim, o numero " + numero + " é maior que 5 .");
+        } else {
+            System.out.println("O numero não é maior que " + numero);
+        }
+    }
+
+}
